5970f302a91400cf1bf8ceb7040aef65f8f58c8a
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-04-24 00:45:43 +0200

    Add files via upload

--- JavaScript project/app.js
+++ JavaScript project/app.js
@@ -5,16 +5,18 @@ let load = 0
 
 let int = setInterval(blurring, 30)
 
+
 function blurring() {
     load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
 
-if (load > 99) {
-clearInterval(int)
-}
-   loadText.innerText = `${load}%`
-   loadText.getElementsByClassName.opacity = scale(load, 0, 100, 1, 0)
-   bg.style.filter = blur(${scale(load, 0, 100, 30, 0)}px)
-}
-https://stackoverflow.com/questions
-const map = (num, in_min, in_max, out_min, out_max) => { return ( num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min 
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
